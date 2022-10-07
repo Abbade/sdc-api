dd5329b4066d0905801f28f6903702b4b1767a2c
Gabriel Broitman <gabrielbroitmanpinheiro@gmail.com>
2022-10-07 16:18:30 -0300

    Good.

--- src/modules/lotes/UseCases/getAllLotes/GetAllLotesController.ts
+++ src/modules/lotes/UseCases/getAllLotes/GetAllLotesController.ts
@@ -3,15 +3,15 @@ import { GetAllLotesUseCase } from './GetAllLotesUseCase';
 
 export class GetAllLotesController {
   async handle(request: Request, response: Response) {
-    const { name, description } = request.body;
+    const { name, page, limit } = request.query as any
+
 
     const getAllLotesUseCase = new GetAllLotesUseCase();
     const result = await getAllLotesUseCase.execute({
       name,
-      description,
-      
+      page,
+      limit
     });
-
     return response.json(result);
   }
 }
--- src/modules/lotes/UseCases/getAllLotes/GetAllLotesUseCase.ts
+++ src/modules/lotes/UseCases/getAllLotes/GetAllLotesUseCase.ts
@@ -2,20 +2,41 @@ import { hash } from 'bcrypt';
 import { prisma } from '../../../../database/prismaClient';
 
 interface ILoteFilter {
-  name: string;
-  description: string;
+  name?: string;
+  description?: string;
+  page: number;
+  limit: number;
 
 }
 
 export class GetAllLotesUseCase {
-  
-  async execute({ name,description }: ILoteFilter) {
+
+  async execute({ name, description, page, limit }: ILoteFilter) {
+
+    console.log(page);
+    console.log(limit);
+    
+
+    const total = await prisma.lotes.count({
+      where: {
+        name: {
+          contains: name
+        }
+      }
+    })
     const lotes = await prisma.lotes.findMany({
+      take: (limit * page)?(limit * page):20,
+      skip: (page - 1)?(page - 1):0,
+      where: {
+        name: {
+          contains: name
+        }
+      },
       include: {
         location: true,
         genetic: true,
         propagationType: true
-        
+
       }
     });
 
@@ -24,6 +45,10 @@ export class GetAllLotesUseCase {
     }
 
 
-    return lotes;
+    return {
+      total,
+      itens: lotes
+    }
+      ;
   }
 }

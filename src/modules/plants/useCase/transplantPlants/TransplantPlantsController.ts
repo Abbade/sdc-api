5bbc801416552f09f1c7b456e06be5c4ef53ced9
Gabriel Abbade <gabbade@gmail.com>
2022-12-21 22:03:09 -0300

    action type

--- src/modules/plants/useCase/transplantPlants/TransplantPlantsController.ts
+++ src/modules/plants/useCase/transplantPlants/TransplantPlantsController.ts
@@ -1,16 +1,35 @@
-import { Request, Response } from 'express';
-import { TransplantPlantsUseCase } from './TransplantPlantsUseCase';
+import { Request, Response } from "express";
+import { TransplantPlantsUseCase } from "./TransplantPlantsUseCase";
 
 export class TransplantPlantsController {
   async handle(request: Request, response: Response) {
-    const { transplantDate, plants, id_recipiente, id_location, id_faseCultivo, obs} = request.body;
-    const id_user_create = request.id_user
+    const {
+      transplantDate,
+      plants,
+      id_recipiente,
+      id_location,
+      id_faseCultivo,
+      obs,
+    } = request.body;
+    const id_user_create = request.id_user;
 
     const transplantPlantsUseCase = new TransplantPlantsUseCase();
-    const result = await transplantPlantsUseCase.execute({
-      transplantDate, plants, id_recipiente, id_location, id_faseCultivo ,id_user_create, obs
-    });
+    if (id_recipiente != null && transplantDate != null) {
+      const result = await transplantPlantsUseCase.execute({
+        transplantDate,
+        plants,
+        id_recipiente,
+        id_location,
+        id_faseCultivo,
+        id_user_create,
+        obs,
+      });
 
-    return response.json(result);
+      return response.json(result);
+    } else {
+      throw new Error(
+        "Deve conter um recipiente e uma data para efetuar o transplante"
+      );
+    }
   }
 }
--- src/modules/plants/useCase/transplantPlants/TransplantPlantsUseCase.ts
+++ src/modules/plants/useCase/transplantPlants/TransplantPlantsUseCase.ts
@@ -36,25 +36,25 @@ export class TransplantPlantsUseCase {
   async execute({ transplantDate, plants, id_recipiente, id_location, id_faseCultivo, id_user_create, obs }: ITransplantPlants) {
 
     //VALIDA EXISTENCIA DE CAMPOS
-    const selectedFaseCultivo = await prisma.fasesCultivo.findFirst({
-      where: {
-        id: id_faseCultivo
-      }
-    })
+    // const selectedFaseCultivo = await prisma.fasesCultivo.findFirst({
+    //   where: {
+    //     id: id_faseCultivo
+    //   }
+    // })
 
-    if (!selectedFaseCultivo) {
-      throw new Error('Fase de cultivo não existente: ' + id_faseCultivo);
-    }
+    // if (!selectedFaseCultivo) {
+    //   throw new Error('Fase de cultivo não existente: ' + id_faseCultivo);
+    // }
 
-    const selectedLocation = await prisma.locations.findFirst({
-      where: {
-        id: id_location
-      }
-    })
+    // const selectedLocation = await prisma.locations.findFirst({
+    //   where: {
+    //     id: id_location
+    //   }
+    // })
 
-    if (!selectedLocation) {
-      throw new Error('Local não existente: ' + id_location);
-    }
+    // if (!selectedLocation) {
+    //   throw new Error('Local não existente: ' + id_location);
+    // }
 
     const selectedRecipiente = await prisma.recipientes.findFirst({
       where: {
@@ -97,10 +97,10 @@ export class TransplantPlantsUseCase {
 
       }
 
-      if (plant.id_faseCultivo > selectedFaseCultivo.id) {
-        throw new Error('Não é possivel voltar com plantas para fase anterior.')
+      // if (plant.id_faseCultivo > selectedFaseCultivo.id) {
+      //   throw new Error('Não é possivel voltar com plantas para fase anterior.')
 
-      }
+      // }
 
 
 
@@ -108,178 +108,178 @@ export class TransplantPlantsUseCase {
     })
 
     // ACLIMATAÇÃO
-    if (selectedFaseCultivo.ordem == 2) {
+    // if (selectedFaseCultivo.ordem == 2) {
 
 
 
-      var param = {
-        where: {
-          id: { in: plants }
-        },
-        data: {
-          id_faseCultivo: id_faseCultivo,
-          id_recipiente: id_recipiente,
-          id_location: id_location,
+    //   var param = {
+    //     where: {
+    //       id: { in: plants }
+    //     },
+    //     data: {
+    //       id_faseCultivo: id_faseCultivo,
+    //       id_recipiente: id_recipiente,
+    //       id_location: id_location,
 
-          lastTransplant: transplantDate,
-
-
-
-        }
-      }
-      const updatedPlants = await prisma.plantas.updateMany(param)
+    //       lastTransplant: transplantDate,
 
 
-      const plantsWithEmptyDate = plantsToUpdate.filter(plant => {
-        return !plant.aclimatationDate
-      }).map(plant => {
-        return plant.id
-      })
 
-      const updateDateParams = {
-        where: {
-          id: { in: plantsWithEmptyDate },
-
-        },
-        data: {
-          aclimatationDate: transplantDate,
-          aclimatationLocation: selectedLocation.name,
-          aclimatationRecipient: selectedRecipiente.name,
-
-        }
-      }
-
-      const updatedDatePlants = await prisma.plantas.updateMany(updateDateParams)
+    //     }
+    //   }
+    //   const updatedPlants = await prisma.plantas.updateMany(param)
 
 
-    }
+    //   const plantsWithEmptyDate = plantsToUpdate.filter(plant => {
+    //     return !plant.aclimatationDate
+    //   }).map(plant => {
+    //     return plant.id
+    //   })
+
+    //   const updateDateParams = {
+    //     where: {
+    //       id: { in: plantsWithEmptyDate },
+
+    //     },
+    //     data: {
+    //       aclimatationDate: transplantDate,
+    //       aclimatationLocation: selectedLocation.name,
+    //       aclimatationRecipient: selectedRecipiente.name,
+
+    //     }
+    //   }
+
+    //   const updatedDatePlants = await prisma.plantas.updateMany(updateDateParams)
+
+
+    // }
 
     // VEGETAÇÃO
-    if (selectedFaseCultivo.ordem == 3) {
-      const param = {
-        where: {
-          id: { in: plants }
-        },
-        data: {
-          id_faseCultivo: id_faseCultivo,
-          id_recipiente: id_recipiente,
-          id_location: id_location,
-          lastTransplant: transplantDate
+    // if (selectedFaseCultivo.ordem == 3) {
+    //   const param = {
+    //     where: {
+    //       id: { in: plants }
+    //     },
+    //     data: {
+    //       id_faseCultivo: id_faseCultivo,
+    //       id_recipiente: id_recipiente,
+    //       id_location: id_location,
+    //       lastTransplant: transplantDate
 
-        }
-      }
-      const updatedPlants = await prisma.plantas.updateMany(param)
+    //     }
+    //   }
+    //   const updatedPlants = await prisma.plantas.updateMany(param)
 
-      const plantsWithEmptyDateV1 = plantsToUpdate.filter(plant => {
-        return !plant.vegetationDate
-      }).map(plant => {
-        return plant.id
-      })
-
-
-      const updateDateParamsV1 = {
-        where: {
-          id: { in: plantsWithEmptyDateV1 },
-
-        },
-        data: {
-          vegetationDate: transplantDate,
-          vegetationRecipient: selectedRecipiente.name,
-          vegetationLocation: selectedLocation.name
-
-        }
-      }
-
-      const updatedDatePlantsV1 = await prisma.plantas.updateMany(updateDateParamsV1)
-
-      const plantsWithEmptyDateV2 = plantsToUpdate.filter(plant => {
-        return plant.vegetationDate && !plant.vegetation2Date
-      }).map(plant => {
-        return plant.id
-      })
+    //   const plantsWithEmptyDateV1 = plantsToUpdate.filter(plant => {
+    //     return !plant.vegetationDate
+    //   }).map(plant => {
+    //     return plant.id
+    //   })
 
 
-      const updateDateParamsV2 = {
-        where: {
-          id: { in: plantsWithEmptyDateV2 },
+    //   const updateDateParamsV1 = {
+    //     where: {
+    //       id: { in: plantsWithEmptyDateV1 },
 
-        },
-        data: {
-          vegetation2Date: transplantDate,
-          vegetation2Recipient: selectedRecipiente.name,
-          vegetation2Location: selectedLocation.name
+    //     },
+    //     data: {
+    //       vegetationDate: transplantDate,
+    //       vegetationRecipient: selectedRecipiente.name,
+    //       vegetationLocation: selectedLocation.name
 
-        }
-      }
+    //     }
+    //   }
 
-      const updatedDatePlantsV2 = await prisma.plantas.updateMany(updateDateParamsV2)
+    //   const updatedDatePlantsV1 = await prisma.plantas.updateMany(updateDateParamsV1)
 
-      const plantsWithEmptyDateV3 = plantsToUpdate.filter(plant => {
-        return plant.vegetationDate && plant.vegetation2Date && !plant.vegetation3Date
-      }).map(plant => {
-        return plant.id
-      })
-
-
-      const updateDateParamsV3 = {
-        where: {
-          id: { in: plantsWithEmptyDateV3 },
-
-        },
-        data: {
-          vegetation3Date: transplantDate,
-          vegetation3Recipient: selectedRecipiente.name,
-          vegetation3Location: selectedLocation.name
-
-        }
-      }
-
-      const updatedDatePlantsV3 = await prisma.plantas.updateMany(updateDateParamsV3)
+    //   const plantsWithEmptyDateV2 = plantsToUpdate.filter(plant => {
+    //     return plant.vegetationDate && !plant.vegetation2Date
+    //   }).map(plant => {
+    //     return plant.id
+    //   })
 
 
+    //   const updateDateParamsV2 = {
+    //     where: {
+    //       id: { in: plantsWithEmptyDateV2 },
+
+    //     },
+    //     data: {
+    //       vegetation2Date: transplantDate,
+    //       vegetation2Recipient: selectedRecipiente.name,
+    //       vegetation2Location: selectedLocation.name
+
+    //     }
+    //   }
+
+    //   const updatedDatePlantsV2 = await prisma.plantas.updateMany(updateDateParamsV2)
+
+    //   const plantsWithEmptyDateV3 = plantsToUpdate.filter(plant => {
+    //     return plant.vegetationDate && plant.vegetation2Date && !plant.vegetation3Date
+    //   }).map(plant => {
+    //     return plant.id
+    //   })
 
 
-    }
+    //   const updateDateParamsV3 = {
+    //     where: {
+    //       id: { in: plantsWithEmptyDateV3 },
+
+    //     },
+    //     data: {
+    //       vegetation3Date: transplantDate,
+    //       vegetation3Recipient: selectedRecipiente.name,
+    //       vegetation3Location: selectedLocation.name
+
+    //     }
+    //   }
+
+    //   const updatedDatePlantsV3 = await prisma.plantas.updateMany(updateDateParamsV3)
+
+
+
+
+    // }
 
     // FLORACAO
-    if (selectedFaseCultivo.ordem == 4) {
-      const param = {
-        where: {
-          id: { in: plants }
-        },
-        data: {
-          id_faseCultivo: id_faseCultivo,
-          id_recipiente: id_recipiente,
-          id_location: id_location,
-          lastTransplant: transplantDate
-        }
-      }
-      const updatedPlants = await prisma.plantas.updateMany(param)
+    // if (selectedFaseCultivo.ordem == 4) {
+    //   const param = {
+    //     where: {
+    //       id: { in: plants }
+    //     },
+    //     data: {
+    //       id_faseCultivo: id_faseCultivo,
+    //       id_recipiente: id_recipiente,
+    //       id_location: id_location,
+    //       lastTransplant: transplantDate
+    //     }
+    //   }
+    //   const updatedPlants = await prisma.plantas.updateMany(param)
 
-      const plantsWithEmptyDate = plantsToUpdate.filter(plant => {
-        return !plant.floweringDate
-      }).map(plant => {
-        return plant.id
-      })
-
-
-      const updateDateParams = {
-        where: {
-          id: { in: plantsWithEmptyDate },
-
-        },
-        data: {
-          floweringDate: transplantDate,
-          floweringLocation: selectedLocation.name,
-          floweringRecipient: selectedRecipiente.name,
-
-        }
-      }
-
-      // const updatedDatePlants = await prisma.plantas.updateMany(updateDateParams)
+    //   const plantsWithEmptyDate = plantsToUpdate.filter(plant => {
+    //     return !plant.floweringDate
+    //   }).map(plant => {
+    //     return plant.id
+    //   })
 
 
-    }
+    //   const updateDateParams = {
+    //     where: {
+    //       id: { in: plantsWithEmptyDate },
+
+    //     },
+    //     data: {
+    //       floweringDate: transplantDate,
+    //       floweringLocation: selectedLocation.name,
+    //       floweringRecipient: selectedRecipiente.name,
+
+    //     }
+    //   }
+
+    //   // const updatedDatePlants = await prisma.plantas.updateMany(updateDateParams)
+
+
+    // }
 
     let actions = [] as any;
 
